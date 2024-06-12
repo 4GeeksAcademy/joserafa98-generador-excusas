8c4f59415a094d3e95b54377de98710d152a82b8
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-06-12 19:08:12 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  function GeneradorExcusas() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car", "the money"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while i was"
+    ];
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    let GeneradorExcusas =
+      who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex];
+    return GeneradorExcusas;
+  }
+
+  document.querySelector("#botonresultado").addEventListener("click", () => {
+    let resultado = document.querySelector("#excuse");
+    resultado.innerHTML = GeneradorExcusas();
+  });
+  console.log();
 };
